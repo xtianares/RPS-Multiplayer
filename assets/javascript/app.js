060468e0cb7abd7899da8ed4f9b98857ba8ed65f
xtianares <xtianares@yahoo.com>
2019-01-19 16:48:39 -0500

    partially working adding a player

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,9 +9,32 @@ var config = {
 };
 firebase.initializeApp(config);
 
+// setting firebase variables
 let database = firebase.database(),
     connectionsRef = database.ref("/connections"),
+    playersRef = database.ref("/players");
+    chatRef = database.ref("/chat");
     connectedRef = database.ref(".info/connected");
+
+// setting game variables
+let player1 = null,
+    player2 = null,
+    playerName = null,
+    player1choice = '',
+    player12hoice = '',
+    player1Details = {
+        name: "",
+        choice: "",
+        wins: 0,
+        losses: 0
+    },
+    player2Details = {
+        name: "",
+        choice: "",
+        wins: 0,
+        losses: 0
+    },
+    playerDetails = null;
 
 connectedRef.on("value", function(snap) {
     if (snap.val()) {
@@ -20,17 +43,70 @@ connectedRef.on("value", function(snap) {
     }
 });
 
-connectionsRef.on("value", function(snap) {
-    console.log(snap.numChildren())
+// trigger to create/login new player
+$('.name-form').on('click', 'button', function(event) {
+    event.preventDefault();
+    playerName = $("#player-name").val().trim();
+    if (playerName !== "" && !(player1 && player2) ) {
+        if (player1 === null) {
+            console.log("added player1");
+            player1Details.name = playerName;
+            database.ref().child("/players/player1").set(player1Details);
+            database.ref("/players/player1").onDisconnect().remove();
+        }
+        else if (player2 === null) {
+            console.log("added player2");
+            player2Details.name = playerName;
+            database.ref().child("/players/player2").set(player2Details);
+            database.ref("/players/player2").onDisconnect().remove();
+        }
+
+        let msg = playerName + " has joined!",
+            chatKey = database.ref().child("/chat/").push().key;
+
+        database.ref("/chat/" + chatKey).set(msg);
+
+        $("#player-name").val("");
+    }
+});
+
+// playersRef.on("child_added", function (snap) {
+//     var chatMsg = snap.val();
+//     var chatEntry = $("<div>").html(chatMsg);
+//
+//     $(".chat-display").append(chatEntry);
+//     $(".chat-display").scrollTop($(".chat-display")[0].scrollHeight);
+// });
+
+chatRef.on("child_added", function (snap) {
+    var chatMsg = snap.val();
+    var chatEntry = $("<div>").html(chatMsg);
+
+    $(".chat-display").append(chatEntry);
+    $(".chat-display").scrollTop($(".chat-display")[0].scrollHeight);
 });
 
 
 
 // trigger to create new player
-$('.name-form').on('click', 'button', function(event) {
-    event.preventDefault();
+// $('.choices').on('mouseenter', 'button', function() {
+//     let pick = $(this).data('pick'),
+//         img = `<img class="pick-img" src="assets/images/${pick}.png">`
+//     $(this).closest('.choices').prev('.card').find('.card-body').empty().append(img);
+// });
+// $('.choices').on("mouseleave", "button", function() {
+//     let pick = $(this).data('pick');
+//     $(this).closest('.choices').prev('.card').find('.card-body').empty();
+// });
 
+// trigger to post player choice
+$('.choices').on("click", "button", function() {
+    event.preventDefault();
+    let pick = $(this).data('pick'),
+        img = `<img class="pick-img" src="assets/images/${pick}.png">`
+    $(this).closest('.choices').prev('.card').find('.card-body').empty().append(img);
 });
+
 
 $('.chat-form').on('click', 'button', function(event) {
     event.preventDefault();
